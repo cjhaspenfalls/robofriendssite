f1920968f542e08c94a6a557401a826a174b8c85
cjhaspenfalls <110737270+cjhaspenfalls@users.noreply.github.com>
2022-11-16 18:27:17 -0500

    changed scroll box height from 60vh to 55vh and increased border size

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '1px solid black', height: '60vh', margin: '10px'}}>
+        <div style={{overflowY: 'scroll', border: '3px solid black', height: '55vh', margin: '10px'}}>
             {props.children}
         </div>
     )
